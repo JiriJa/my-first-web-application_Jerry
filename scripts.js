6a1ed0c312be7dae1b0587608ee3e3289b6fef41
JiriJa <34089657+JiriJa@users.noreply.github.com>
2020-04-15 18:40:23 +0200

    daily update 02

--- scripts.js
+++ scripts.js
@@ -6,11 +6,20 @@ let dataPrvni = {
     description: 'The car is before tire change'
   };
 
-
-
 let dataDruhy = {
     photo: './fotky/IMG_20200410_111004.jpg',
     title: 'PEUGEOT 306, RZ 5S5 4242, color: Cyan',
+    description: 'The car is after tire change'
+  };
+  let dataTreti = {
+    photo: './fotky/IMG_20200410_113026.jpg',
+    title: 'SUZUKI SWIFT, RZ 3AM 6833, color: Racing Red',
+    description: 'The car is before tire change'
+  };
+ 
+  let dataCtvrty = {
+    photo: './fotky/IMG_20200410_124004.jpg',
+    title: 'SUZUKI SWIFT, RZ 3AM 6833, color: Racing Red',
     description: 'The car is after tire change'
   };
 
@@ -22,7 +31,21 @@ let dataDruhy = {
   })
 
   $('#druhy-it').click(() => {
-  $('#id-photo-main').attr('src', dataDruhy.photo);
-  $('#photo-title').text(dataDruhy.title);
-  $('#photo-descripction').text(dataDruhy.description);
-})
+    $('#id-photo-main').attr('src', dataDruhy.photo);
+    $('#photo-title').text(dataDruhy.title);
+    $('#photo-descripction').text(dataDruhy.description);
+  })
+  
+  $('#treti-it').click(() => {
+    $('#id-photo-main').attr('src', dataTreti.photo);
+    $('#photo-title').text(dataTreti.title);
+    $('#photo-descripction').text(dataTreti.description);
+  })
+    
+  $('#ctvrty-it').click(() => {
+    $('#id-photo-main').attr('src', dataCtvrty.photo);
+    $('#photo-title').text(dataCtvrty.title);
+    $('#photo-descripction').text(dataCtvrty.description);
+  })
+
+
