30721ae2884ce0e4a64c5a15af7aaadd79d772b7
JiriJa <34089657+JiriJa@users.noreply.github.com>
2020-04-16 16:44:40 +0200

    Today progress
    Full finctionality is done :-)

--- scripts.js
+++ scripts.js
@@ -38,44 +38,93 @@ let dataDruhy = {
 
   
   $('#prvni-it').click(() => {
-    $('#id-photo-main').attr('src', dataPrvni.photo);
-    $('#photo-title').text(dataPrvni.title);
-    $('#photo-descripction').text(dataPrvni.description);
+    // $('#id-photo-main').attr('src', dataPrvni.photo);
+    // $('#photo-title').text(dataPrvni.title);
+    // $('#photo-descripction').text(dataPrvni.description);
+    currentPhoto = 0;
+    loadPhoto(currentPhoto);
   })
   
   $('#druhy-it').click(() => {
-    $('#id-photo-main').attr('src', dataDruhy.photo);
-    $('#photo-title').text(dataDruhy.title);
-    $('#photo-descripction').text(dataDruhy.description);
+    // $('#id-photo-main').attr('src', dataDruhy.photo);
+    // $('#photo-title').text(dataDruhy.title);
+    // $('#photo-descripction').text(dataDruhy.description);
+     currentPhoto = 1;
+    loadPhoto(currentPhoto);   
   })
 
    $('#treti-it').click(() => {
-    $('#id-photo-main').attr('src', dataTreti.photo);
-    $('#photo-title').text(dataTreti.title);
-    $('#photo-descripction').text(dataTreti.description);
+    // $('#id-photo-main').attr('src', dataTreti.photo);
+    // $('#photo-title').text(dataTreti.title);
+    // $('#photo-descripction').text(dataTreti.description);
+    currentPhoto = 2;
+    loadPhoto(currentPhoto);
   })
 
    $('#ctvrty-it').click(() => {
-    $('#id-photo-main').attr('src', dataCtvrty.photo);
-    $('#photo-title').text(dataCtvrty.title);
-    $('#photo-descripction').text(dataCtvrty.description);
+    // $('#id-photo-main').attr('src', dataCtvrty.photo);
+    // $('#photo-title').text(dataCtvrty.title);
+    // $('#photo-descripction').text(dataCtvrty.description);
+    currentPhoto = 3;
+    loadPhoto(currentPhoto);
   })
   
   $('#paty-it').click(() => {
-    $('#id-photo-main').attr('src', dataPaty.photo);
-    $('#photo-title').text(dataPaty.title);
-    $('#photo-descripction').text(dataPaty.description);
+    // $('#id-photo-main').attr('src', dataPaty.photo);
+    // $('#photo-title').text(dataPaty.title);
+    // $('#photo-descripction').text(dataPaty.description);
+    currentPhoto = 4;
+    loadPhoto(currentPhoto);
   })
     
   $('#sesty-it').click(() => {
-    $('#id-photo-main').attr('src', dataSesty.photo);
-    $('#photo-title').text(dataSesty.title);
-    $('#photo-descripction').text(dataSesty.description);
+    // $('#id-photo-main').attr('src', dataSesty.photo);
+    // $('#photo-title').text(dataSesty.title);
+    // $('#photo-descripction').text(dataSesty.description);
+    currentPhoto = 5;
+    loadPhoto(currentPhoto);
   })
       
   $('#posledni-it').click(() => {
-    $('#id-photo-main').attr('src', dataPosledni.photo);
-    $('#photo-title').text(dataPosledni.title);
-    $('#photo-descripction').text(dataPosledni.description);
+    // $('#id-photo-main').attr('src', dataPosledni.photo);
+    // $('#photo-title').text(dataPosledni.title);
+    // $('#photo-descripction').text(dataPosledni.description);
+    currentPhoto = 6;
+    loadPhoto(currentPhoto);
+  })
+
+
+
+
+  let currentPhoto = 0;
+  let imagesData = [dataPrvni, dataDruhy, dataTreti, dataCtvrty, dataPaty, dataSesty, dataPosledni];
+
+  $('#id-photo-main').attr('src', imagesData[currentPhoto].photo);
+
+  let loadPhoto = (photoNumber) => {
+    $('#id-photo-main').attr('src', imagesData[photoNumber].photo);
+    // ...
+  }
+
+  
+  loadPhoto(currentPhoto);
+
+  $('#sipka-vpravo').click(() => {
+    currentPhoto++;
+     if (currentPhoto >= 7) {
+      currentPhoto = 0;
+    }   
+    loadPhoto(currentPhoto);
+  })
+
+
+
+  
+  $('#sipka-vlevo').click(() => {
+    (currentPhoto = currentPhoto - 1);
+    if (currentPhoto <= -1) {
+      currentPhoto = 6;
+    }    
+    loadPhoto(currentPhoto);
   })
 
